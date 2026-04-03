223492eb9002eb96de002737b9e75914ff8ec529
Kayra <kss367@drexel.edu>
2026-04-03 13:15:54 -0400

    templates/static folder structure

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from simplewriting.analysis import markov_generation
+from simplewriting.analysis import clean_text
+from simplewriting.analysis import load_text
 
-if __name__ == "__main__":
-    print()
+@app.route('/')
+def analyze():
+    text = load_text(data/dostoevsky.txt)
+    words = clean_text(text)
+    return render_template(
+        "index.html",
+        generated_text=markov_generation(words, length=10)
+    )
