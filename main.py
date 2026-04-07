d20ae97b29782e971fff3768a8dc9facea8c1994
Kayra <kss367@drexel.edu>
2026-04-07 12:34:18 -0400

    added in more .txt files; changed color styling; updated useage of .analysis to use a chain

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 # add parent directory to Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from simplewriting.analysis import markov_generation, clean_text, load_text
+from simplewriting.analysis import markov_generation, create_chain, clean_text, load_text
 from flask import Flask, render_template, redirect, url_for, jsonify
 
 app = Flask(__name__)
@@ -14,17 +14,18 @@ base_dir = os.path.dirname(__file__)
 file_path = os.path.join(base_dir, "data", "dostoyevsky.txt")
 text = load_text(file_path)
 words = clean_text(text)
+chain=create_chain(words)
 
 @app.route('/')
 def index():
     return render_template(
         "index.html",
-        generated_text=markov_generation(words, length=10)
+        generated_text=markov_generation(words, chain, length=10)
     )
 
 @app.route('/quote')
 def quote():
-    new_text = markov_generation(words, length=10)
+    new_text = markov_generation(words, chain, length=10)
     return jsonify({'generated_text': new_text})
 
 if __name__ == "__main__":
